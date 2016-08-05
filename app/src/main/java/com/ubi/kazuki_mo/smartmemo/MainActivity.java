d1d5719b4a9fa4f4853c1c6ad8840739c664e4c4
kazuki-mo <moriya.kazuki.228@gmail.com>
2016-08-05 13:45:35 +0900

    firstCommit

--- app/src/main/java/com/ubi/kazuki_mo/smartmemo/MainActivity.java
+++ app/src/main/java/com/ubi/kazuki_mo/smartmemo/MainActivity.java
@@ -18,7 +18,7 @@ import android.content.Context;
 
 public class MainActivity extends AppCompatActivity{
 
-    // 定数,変数,インスタンス
+    // 定数,変数,インスタンスなど
     private static final int REQUEST_CODE = 0;
     private TextView TV_VoiceRecogResult;
     private ListView LV_Memo;
